334e37c42d8c0c2623c52db4df9eb82847965424
Estudo Kotlin Android <mr6227017@gmail.com>
2026-01-27 22:02:22 -0300

    feat: implement data persistence and repository logic

--- app/src/main/java/com/example/mymercado/core/data/CarrinhoEntity.kt
+++ app/src/main/java/com/example/mymercado/core/data/CarrinhoEntity.kt
@@ -5,11 +5,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "carrinho")
 data class CarrinhoEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val produtoId: Int,
     val usuarioEmail: String,
-    val quantidade: Int,
+    val titulo: String,
     val precoNoMomento: Double,
-    val titulo: String, // Adicionado para facilitar exibição
-    val urlImagem: String // Adicionado para exibir a foto no carrinho
+    val urlImagem: String,
+    val quantidade: Int,
+    val vendedorNome: String // Adicionado para exibir a foto no carrinho
 )
